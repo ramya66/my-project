af63785d25753a7ebe07195a9ac5ff906653b0cf
ramya <ramyasri.kurapati@gmail.com>
2016-09-25 10:34:33 +0530

    hi

---
+++ x.py
@@ -0,0 +1,22 @@
+from flask import Flask, request,url_for,render_template 
+
+
+app=Flask(__name__)
+
+
+
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+if __name__ =='__main__':
+    app.run()
